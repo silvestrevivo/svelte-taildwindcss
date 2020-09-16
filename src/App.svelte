7e0bd8168bef5a63925e0cefa412120e2df20081
silvestrevivo <silvestrevivomillan@gmail.com>
2020-09-16 09:00:52 +0200

    first approach to pattern

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,60 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
+<style global>
+  /* purgecss start ignore */
+  @tailwind base;
+  @tailwind components;
+  /* purgecss end ignore */
+  @tailwind utilities;
+</style>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <p class="p-2">
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<!--<script>
+  export let name;
+</script><style global>
+  /* purgecss start ignore */
+  @tailwind base;
+  @tailwind components;
+  /* purgecss end ignore */
+  @tailwind utilities;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  @layer base {
+    p {
+      @apply text-l;
+    }
+  }
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style><main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main> -->
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// const production = !process.env.ROLLUP_WATCH;
+// module.exports = {
+//   purge: {
+//     content: [
+//       "./src/**/*.svelte",
+//     ],
+//     enabled: production // disable purge in dev
+//   },
+//   future: { // for tailwind 2.0 compat
+//     purgeLayersByDefault: true,
+//     removeDeprecatedGapUtilities: true,
+//   },
+// };
+
+module.exports = {
+  purge: ["./src/**/*.svelte"],
+};
+
